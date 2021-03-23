06f93b0a24f6109a04ed70b438c9403476fdcb8e
Andreas Frederiksen <Andreas020299@gmail.com>
2021-03-23 16:23:57 +0100

    Creation of SPARQL class

---
+++ Triple.js
@@ -0,0 +1,15 @@
+class Triple {
+    subject = ""
+    predicate = ""
+    object = ""
+
+    constructor(subject, predicate, object) {
+        this.subject = subject
+        this.predicate = predicate
+        this.object = object
+    }
+    toString(){
+        return "Subject: " + this.subject + ", predicate: " + this.predicate + " and object: " + this.object
+    }
+}
+
--- sparql.js
+++ sparql.js
@@ -1,27 +1,62 @@
-const fs = require('fs')
-const rdf = require('rdf-ext')
-const N3Parser = require('rdf-parser-n3')
+//TODO: TYPE (SELECT/CONSTRUCT)
+//TODO: BOUND VARIABLE
+//TODO: UNBOUND VARIABLE
+//TODO: LITERAL PREDICATE
+//TODO: PREFIXES
 
-// create N3 parser instance
-const parser = new N3Parser({factory: rdf})
 
-// Read a Turtle file and stream it to the parser
-const quadStream = parser.import(fs.createReadStream('./test.ttl'))
 
-;(async function () {
-  // create a new dataset and import the quad stream into it (reverse pipe) with Promise API
-  const dataset = await rdf.dataset().import(quadStream)
-  dataset.forEach(({ object }) => {
-    console.log(object)
-  })
-  /*
-  console.log(`Dataset contains ${dataset.size} quads`)
+class SPARQL {
+    triples = []
+    type = ""
+    boundVariables = []
+    unboundVariables = []
+    literals = []
+    prefixes = []
 
-  const jobTitles = dataset.match(null, rdf.namedNode('http://schema.org/jobTitle')).toArray()
-  jobTitles.forEach(({ object }) => {
-    console.log(`Sheldon's jobTitle is ${object.value}`)
-  })
 
-  const relations = dataset.match(null, rdf.namedNode('http://schema.org/knows')).toArray()
-  console.log(`Sheldon knows ${relations.map(({ object }) => object.value).join(', ')}`)*/
-})()
+    constructor(triples, type, boundVariables) {
+        this.triples = triples
+        this.type = type
+        this.boundVariables = boundVariables
+        this.setRest()
+    }
+
+    setRest() {
+        for (let i = 0; i < this.triples.length; i++) {
+            for (const [key, value] of Object.entries(triples[i])){
+                this.setUnboundVariables(key, value)
+                this.setLiterals(value)
+                this.setPrefixes(value)
+            }
+
+        }
+    }
+
+    setUnboundVariables(key, value) {
+        if (!this.boundVariables.includes(value) && !this.unboundVariables.includes(value) && key !== "predicate") {
+            this.unboundVariables.push(value)
+        }
+    }
+
+    setLiterals(value) {
+        if (value.includes("\"") && !this.literals.includes(value)) {
+            this.literals.push(value)
+        }
+    }
+
+    setPrefixes(value) {
+        if (value.includes(":") && !this.prefixes.includes(value.split(":")[0] + ":")) {
+            this.prefixes.push(value.split(":")[0] + ":")
+        }
+
+    }
+
+}
+
+let triples = [new Triple("?x", "foaf:name", "?name"), new Triple("?x", "foaf:mbox", "?mbox")]
+let boundVariables = ["?name", "?mbox"]
+let type = "SELECT"
+let sparqltest = new SPARQL(triples, type, boundVariables)
+
+document.write("Prefixes: " + sparqltest.prefixes + "\nLiterals: " + sparqltest.literals + "\nUnboundvariables: " + sparqltest.unboundVariables)
