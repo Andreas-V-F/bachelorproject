c837a22250a7b34a2ebfaa8bcc64ded1dcb969cf
Andreas Timm Adriansen <andreaserchang@gmail.com>
2021-03-30 18:12:13 +0200

    Update sparql.js
    remove triple

--- sparql.js
+++ sparql.js
@@ -3,20 +3,6 @@
 //TODO: UNBOUND VARIABLE
 //TODO: LITERAL PREDICATE
 //TODO: PREFIXES
-class Triple {
-    subject = ""
-    predicate = ""
-    object = ""
-
-    constructor(subject, predicate, object) {
-        this.subject = subject
-        this.predicate = predicate
-        this.object = object
-    }
-    toString(){
-        return "Subject: " + this.subject + ", predicate: " + this.predicate + " and object: " + this.object
-    }
-}
 
 
 class SPARQL {
