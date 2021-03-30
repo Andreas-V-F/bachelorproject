1f0ab8bcad8d87003cf8230805a6788e911dc97b
Andreas Timm Adriansen <andreaserchang@gmail.com>
2021-03-30 23:58:47 +0200

    Nodecreation

--- VisualHandler.js
+++ VisualHandler.js
@@ -4,6 +4,9 @@ class VisualHandler {
     height;
     styleSheet;
     svgElement;
+    nodes = [];
+    posX;
+    posY;
 
     constructor(width, height) {
         this.width = width;
@@ -19,6 +22,7 @@ class VisualHandler {
         svg.setAttribute("viewBox", "0 0 " + this.width + "" + this.width + "");
         document.body.append(svg);
         this.svgElement = svg;
+        this.onRightClick();
     }
 
     styleRules() {
@@ -27,5 +31,32 @@ class VisualHandler {
         this.styleSheet = styleElement.sheet;
         this.styleSheet.insertRule("svg { background-color: #F3F0F0; display: block; margin: auto } ", this.styleSheet.cssRules.length);
     }
+
+    nodeCreation() {
+        var size = 25;
+        var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        circle.setAttribute("cx", ""+ this.posX +"");
+        circle.setAttribute("cy", ""+ this.posY +"");
+        circle.setAttribute("r",  ""+ size +"");
+        circle.setAttribute("stroke", "green")
+        circle.setAttribute("fill", "blue");
+        this.svgElement.appendChild(circle);
+    }
+
+    onRightClick(){
+        this.svgElement.addEventListener('mousedown', (event) => {
+            if(event.button == 2){
+                let point = this.svgElement.createSVGPoint();
+                point.x = event.clientX; 
+                point.y = event.clientY; 
+                point = point.matrixTransform(this.svgElement.getScreenCTM().inverse());
+                this.posX = point.x;
+                this.posY = point.y;
+                console.log("Point x = " + this.posX + ", Point y = " + this.posY);
+                this.nodeCreation();
+            }
+        })
+    }
+
 
 }
