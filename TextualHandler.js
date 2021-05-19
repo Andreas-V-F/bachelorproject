5628e09ea2fb04b8cf22948db515db210bad49cb
Andreas Frederiksen <Andreas020299@gmail.com>
2021-05-19 16:29:04 +0200

    Reformatted texthandler

--- TextualHandler.js
+++ TextualHandler.js
@@ -4,7 +4,7 @@ import {parseToVisual} from "./VisualParser.js";
 let height = "200px"
 let width = "400px"
 
-export function initiateTextualHandler(){
+export function initiateTextualHandler() {
     let div = document.createElement("div")
     div.style.float = "left"
     div.style.marginLeft = "200px"
@@ -17,12 +17,12 @@ export function initiateTextualHandler(){
     parseText.style.height = height
     parseText.style.width = width
     parseText.spellcheck = false
-    parseText.onchange = function (){
+    parseText.onchange = function () {
         parse()
     }
     let parseButton = document.createElement("button")
     parseButton.textContent = "Run"
-    parseButton.onclick = function(){
+    parseButton.onclick = function () {
         parse()
     }
 
@@ -40,49 +40,46 @@ export function initiateTextualHandler(){
 
 
 }
-export function updateTextarea(updatedText){
+
+export function updateTextarea(updatedText) {
     document.getElementById("parse").value = updatedText
 }
 
-function parse()
-{
-    var text =  document.getElementById("parse").value
+function parse() {
+    let text = document.getElementById("parse").value;
     try {
-        var entry = PARSER.parse(text);
+        let entry = PARSER.parse(text);
         parseToVisual(entry)
         document.getElementById("errorConsole").value = "Parse success!!!"
         $('#parse').highlightTextarea('destroy')
     } catch (err) {
-            var start
-            var end
-            if(err.location.start.offset + findWordLength(text, err.location.start.offset) >= text.length){
+        let start;
+        let end;
+        if (err.location.start.offset + findWordLength(text, err.location.start.offset) >= text.length) {
             start = text.length - 2
             end = text.length - 1
+        } else if (err.found === "\n") {
+            start = err.location.start.offset - 1
+            end = err.location.start.offset + findWordLength(text, err.location.start.offset) + 2
+        } else {
+            start = err.location.start.offset
+            end = err.location.start.offset + findWordLength(text, err.location.start.offset)
+            if (start === end) {
+                end += 1
             }
-            else if(err.found === "\n") {
-                start = err.location.start.offset -1
-                end = err.location.start.offset + findWordLength(text, err.location.start.offset) + 2
-            } else
-            {
-                start = err.location.start.offset
-                end = err.location.start.offset + findWordLength(text, err.location.start.offset)
-                if(start === end){
-                    end+=1
-                }
-            }
+        }
 
-            document.getElementById("errorConsole").value = "Line " + err.location.start.line + "," + " column " + err.location.start.column + ": " + err
-            $('#parse').highlightTextarea('destroy')
-            $("#parse").highlightTextarea({
-                color: "#FF0000",
+        document.getElementById("errorConsole").value = "Line " + err.location.start.line + "," + " column " + err.location.start.column + ": " + err
+        $('#parse').highlightTextarea('destroy')
+        $("#parse").highlightTextarea({
+            color: "#FF0000",
             ranges: [[start, end]]
-            });
+        });
     }
     document.getElementById("parse").focus()
-
 }
 
-function findWordLength(text, index){
+function findWordLength(text, index) {
 
     return text.substring(index).split(" ").join("\n").split("\n")[0].length
 }
